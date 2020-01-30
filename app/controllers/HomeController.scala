a021b2ad7bd0e1f5b64b1403c5f807dda937a33e
sicLotus <lotus_mud@gmx.de>
2020-01-30 14:52:48 +0100

    load and show user data from api

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,15 +2,19 @@ package controllers
 
 import javax.inject._
 import play.api._
+import play.api.libs.ws.WSClient
 import play.api.mvc._
+
+import scala.concurrent.ExecutionContext
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-
+class HomeController @Inject()(val controllerComponents: ControllerComponents,
+                               val ws: WSClient,
+                               implicit val ec: ExecutionContext) extends BaseController {
   /**
    * Create an Action to render an HTML page.
    *
@@ -18,7 +22,11 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def index(): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
+    val url = "http://jsonplaceholder.typicode.com/users/1"
+    val futureResult = ws.url(url).get().map { response =>
+      Ok(views.html.index(response.json.toString))
+    }
+    futureResult
   }
 }
--- build.sbt
+++ build.sbt
@@ -8,6 +8,7 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.13.1"
 
 libraryDependencies += guice
+libraryDependencies += ws
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
 
 // Adds additional packages into Twirl
